5ca4825be7e5e52c840aaedd31f65e231662a88e
El Houssin <boubiahoussin@gmail.com>
2024-06-23 22:52:39 +0200

    change database

--- auth-service/src/main/java/com/medhead/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/medhead/authservice/AuthServiceApplication.java
@@ -2,11 +2,15 @@ package com.medhead.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.medhead.authservice.repository")
+@EntityScan(basePackages = "com.medhead.authservice.model")
+
 public class AuthServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
--- auth-service/src/main/java/com/medhead/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/medhead/authservice/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.medhead.authservice.repository;
 
 import com.medhead.authservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
 
