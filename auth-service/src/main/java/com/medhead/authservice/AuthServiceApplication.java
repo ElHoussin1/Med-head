072e9cc06908772e998ec0b869a5e2b776a62661
ElHoussin1 <boubiahoussin@gmail.com>
2024-07-04 03:43:15 +0200

    1st changing database

--- auth-service/src/main/java/com/medhead/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/medhead/authservice/AuthServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.medhead.authservice.repository")
 @EntityScan(basePackages = "com.medhead.authservice.model")
-
 public class AuthServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
